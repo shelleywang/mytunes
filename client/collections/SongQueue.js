4db10a6735a3401a546b5f6179e1090c6c78b48e
shelleywang <shelleyw13@gmail.com>
2015-07-15 17:31:01 -0700

    Implements models and views for app and library

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,7 +1,32 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Songs.extend({
 
-  initialize: function(){
-  }
+  initialize: function(songs){
+    this.add(songs);
+
+    this.on('add', function(model) {
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    }, this);
+
+    this.on('ended', function(model) {
+      this.shift();
+      if (this.first() !== undefined) {
+        this.playFirst();
+      }
+    }, this);
+
+    this.on('dequeue', function(model) {
+      this.remove(model);
+    }, this);
+
+  },
+
+  playFirst: function() {
+    this.first().play();
+
+  },
+
 
 });
